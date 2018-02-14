65245cdc936b566f852c341ccae5f9a6b416461b
Alex Forrence <aforren1@jhu.edu>
2018-02-14 07:07:43 -0500

    Generate practice blocks

--- tables/mkblocks.py
+++ tables/mkblocks.py
@@ -12,11 +12,12 @@ def mk_block(name='testx.csv', pair=[0, 4], num_trials=120,
     if num_switch_trials % 2 != 0:
         raise ValueError('Make sure both directions are sampled evenly.')
 
-    min_frames = int(min_max_time[0] * frame_rate) # assume 60 fps
+    min_frames = int(min_max_time[0] * frame_rate)  # assume 60 fps
     max_frames = int(min_max_time[1] * frame_rate)
 
-    switch_times = np.random.randint(min_frames, max_frames + 1, num_switch_trials).astype('d')
-    switch_times /= frame_rate # recover time in seconds
+    switch_times = np.random.randint(
+        min_frames, max_frames + 1, num_switch_trials).astype('d')
+    switch_times /= frame_rate  # recover time in seconds
     # otherwise, switch times are zero
     pair = [float(pair[0]), float(pair[1])]
     pair_1 = pair
@@ -30,10 +31,14 @@ def mk_block(name='testx.csv', pair=[0, 4], num_trials=120,
     pairs_3 = np.tile(pair_3, (num_other_trials//2, 1))
     pairs_4 = np.tile(pair_4, (num_other_trials // 2, 1))
 
-    pairs_1 = np.concatenate((pairs_1, switch_times[:num_switch_trials//2, None]), axis=1)
-    pairs_2 = np.concatenate((pairs_2, switch_times[-num_switch_trials//2:, None]), axis=1)
-    pairs_3 = np.concatenate((pairs_3, np.zeros((num_other_trials//2, 1))), axis=1)
-    pairs_4 = np.concatenate((pairs_4, np.zeros((num_other_trials//2, 1))), axis=1)
+    pairs_1 = np.concatenate(
+        (pairs_1, switch_times[:num_switch_trials//2, None]), axis=1)
+    pairs_2 = np.concatenate(
+        (pairs_2, switch_times[-num_switch_trials//2:, None]), axis=1)
+    pairs_3 = np.concatenate(
+        (pairs_3, np.zeros((num_other_trials//2, 1))), axis=1)
+    pairs_4 = np.concatenate(
+        (pairs_4, np.zeros((num_other_trials//2, 1))), axis=1)
 
     pair_3.append(0)
     pair_4.append(0)
@@ -41,14 +46,18 @@ def mk_block(name='testx.csv', pair=[0, 4], num_trials=120,
     all_pairs = np.vstack((pairs_1, pairs_2, pairs_3, pairs_4))
     np.random.shuffle(all_pairs)
     all_pairs = np.vstack((practice, all_pairs))
-    pair_frame = pd.DataFrame(all_pairs, columns = ['first', 'second', 'switch_time'])
+    pair_frame = pd.DataFrame(
+        all_pairs, columns=['first', 'second', 'switch_time'])
     pair_frame.to_csv(name, index=False, float_format='%.4f')
+
 
 def mk_blocks(fingers=[0, 4, 5, 9], blocks_per_pair=2):
     pairs = [[a, b] for a in fingers for b in fingers if (a != b & b < a)]
     np.random.shuffle(pairs)
     pairs = np.repeat(pairs, blocks_per_pair, 0)
+    mk_block('block0_demo.csv', pair=pairs[0], num_trials=50, prop_switch=0)
     for i, pair in enumerate(pairs):
-        mk_block('block' + str(i) + '.csv', pair=pair)
+        mk_block('block' + str(i + 1) + '.csv', pair=pair)
+
 
 mk_blocks()
--- tables/mkmultiblocks.py
+++ tables/mkmultiblocks.py
@@ -4,7 +4,7 @@ import numpy as np
 import pandas as pd
 
 
-def mk_multi_block(name='testy.csv', combo=[0, 4, 5, 9], num_trials=120, 
+def mk_multi_block(name='testy.csv', combo=[0, 4, 5, 9], num_trials=120,
                    prop_switch=0.3, min_max_time=(0.1, 0.45), frame_rate=60):
     num_switch_trials = int(num_trials * prop_switch)
     num_other_trials = int(num_trials - num_switch_trials)
@@ -17,7 +17,8 @@ def mk_multi_block(name='testy.csv', combo=[0, 4, 5, 9], num_trials=120,
         raise ValueError('Make sure all selections are sampled evenly.')
     min_frames = int(min_max_time[0] * frame_rate)
     max_frames = int(min_max_time[1] * frame_rate)
-    switch_times = np.random.randint(min_frames, max_frames + 1, num_switch_trials).astype('d')
+    switch_times = np.random.randint(
+        min_frames, max_frames + 1, num_switch_trials).astype('d')
     switch_times /= frame_rate
     tmp = list()
     count = 0
@@ -27,7 +28,8 @@ def mk_multi_block(name='testy.csv', combo=[0, 4, 5, 9], num_trials=120,
             rr = np.concatenate((rr, np.zeros((rr.shape[0], 1))), axis=1)
         else:
             rr = np.tile(x, (num_switch_trials//not_matching, 1))
-            rr = np.concatenate((rr, switch_times[count:count+rr.shape[0], None]), axis=1)
+            rr = np.concatenate(
+                (rr, switch_times[count:count+rr.shape[0], None]), axis=1)
             count += rr.shape[0]
         tmp.append(rr)
     all_pairs = np.vstack(tmp)
@@ -36,11 +38,16 @@ def mk_multi_block(name='testy.csv', combo=[0, 4, 5, 9], num_trials=120,
     for i in practice:
         i.append(0)
     all_pairs = np.vstack((practice, all_pairs))
-    pair_frame = pd.DataFrame(all_pairs, columns=['first', 'second', 'switch_time'])
+    pair_frame = pd.DataFrame(
+        all_pairs, columns=['first', 'second', 'switch_time'])
     pair_frame.to_csv(name, index=False, float_format='%.4f')
 
+
 def mk_multi_blocks(fingers=[0, 4, 5, 9], blocks=8):
-    for i in range(blocks):
-        mk_multi_block('multiblock' + str(i) + '.csv', combo=fingers)
+    mk_multi_block('multiblock0_demo.csv', combo=fingers,
+                   num_trials=50, prop_switch=0)
+    for i in range(blocks - 1):
+        mk_multi_block('multiblock' + str(i + 1) + '.csv', combo=fingers)
+
 
 mk_multi_blocks()
